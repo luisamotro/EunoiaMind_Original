cdc7ef5dc64fb0de36ab55c9d7c785f4f825bf23
luisamotro <luisamotro10@gmail.com>
2022-11-14 15:45:10 -0300

    luisa

---
+++ src/main/java/cl/generation/web/models/Usuario.java
@@ -0,0 +1,10 @@
+package cl.generation.web.models;
+
+public class Usuario {
+	private Long id;
+	private String nombre;
+	private String correo;
+	private String password;
+	private String password3;
+
+}

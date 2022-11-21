c8765ac796f0756cfdbeece91f8b2df3fbbe97d0
Atenas Pérez <97369170+At3nas@users.noreply.github.com>
2022-11-21 12:22:08 -0300

    Getter y setter con Lombok

--- src/main/java/cl/generation/web/models/Usuario.java
+++ src/main/java/cl/generation/web/models/Usuario.java
@@ -1,6 +1,7 @@
 package cl.generation.web.models;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,13 +16,26 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
-@Table(name="usuarios")
+@Table(name = "usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotNull
 	private String nombreUsuario;
 	@NotNull
@@ -30,85 +44,22 @@ public class Usuario {
 	private String contraseña;
 	@Transient
 	private String contraseña2;
-	
+
 	@Column(updatable = false)
-	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createdAt;
-	
-	
-	@DateTimeFormat(pattern="yyyy-MM-dd")
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date updatedAt;
-	
-	
-	public Usuario() {
-		super();
-	}
 
-	public Usuario(Long id, String nombreUsuario, String correo, String contraseña, String contraseña2) {
-		super();
-		this.id = id;
-		this.nombreUsuario = nombreUsuario;
-		this.correo = correo;
-		this.contraseña = contraseña;
-		this.contraseña2 = contraseña2;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombreUsuario() {
-		return nombreUsuario;
-	}
-
-	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
-	}
-
-	public String getCorreo() {
-		return correo;
-	}
-
-	public void setCorreo(String correo) {
-		this.correo = correo;
-	}
-
-	public String getContraseña() {
-		return contraseña;
-	}
-
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
-	}
-
-	public String getContraseña2() {
-		return contraseña2;
-	}
-
-	public void setContraseña2(String contraseña2) {
-		this.contraseña2 = contraseña2;
-	}
-	
 	@PrePersist
-    protected void onCreate(){
-        this.createdAt = new Date();
-    }
-	
-	@PreUpdate
-    protected void onUpdate(){
-        this.updatedAt = new Date();
-    }
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
 
-	
-	
-	
-	
-	
-	
-	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
 
 }
